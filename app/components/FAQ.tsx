1278247cf4c7de8b8ce46d5f7a49549a71862da0
Raghu Anand <raghuaanand@gmail.com>
2024-09-16 01:43:34 +0530

    done with footer comp

--- app/components/FAQ.tsx
+++ app/components/FAQ.tsx
@@ -2,10 +2,12 @@ import FaqList from "./FaqList"
 
 export default function FAQ() {
     return (
-        <div className="bg-[#fcf9f9] flex flex-col justify-center items-center gap-3 font-rubik px-5 pt-20">
-            <h1 className="text-myGreen font-extrabold leading-[14.3px] text-[14.3px]">FAQ</h1>
-            <h2 className="font-extrabold text-[29.7px] text-center leading-[29.7px]">GOT QUESTIONS?<span className="block">WE'VE GOT</span>  <span className="text-myGreen underline">ANSWERS!</span> </h2>
-            <p className="text-center font-[500] text-[12.1px] px-2 padding-[16.46px] opacity-75">We’re here to help! Here are some of the most common questions about BloxReward. If you don’t see your question here, feel free to reach out to us anytime.</p>
+        <div className="bg-[#fcf9f9]  font-rubik px-5 pt-20">
+            <div className="flex flex-col justify-center items-center gap-3  ">
+                <h1 className="text-myGreen font-extrabold leading-[14.3px] text-[14.3px] md:text-[26px] md:leading-[26px] ">FAQ</h1>
+                <h2 className="font-extrabold text-[29.7px] text-center leading-[29.7px] md:w-[614px] md:text-[54px] md:leading-[54px] ">GOT QUESTIONS? WE'VE GOT<span className="underline text-myGreen md:decoration-[3px]"> ANSWERS!</span> </h2>
+                <p className="text-center font-[500] text-[12.1px] px-2 padding-[16.46px] opacity-75 md:text-[22px] md:leading-[29.92px] md:w-[1217px] ">We’re here to help! Here are some of the most common questions about BloxReward. If you don’t see your question here, feel free to reach out to us anytime.</p>
+            </div>
 
             <div>
                 <FaqList />
--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,45 +6,49 @@ import { FaYoutube } from "react-icons/fa";
 export default function Footer() {
     return (
         <div className="bg-bgColor flex flex-col px-6 gap-5 pb-8 ">
-            <div>
-                <Image src={'/footer_logo.png'} alt="footer logo" width={309} height={56} />
-            </div>
-            <div className="flex gap-5">
-                <div className="w-[33.5px] h-[33.5px] rounded-lg bg-green-100 flex items-center justify-center">
-                    <a href="www.discord.com"><FaDiscord /></a>
+            <div className="md:flex md:justify-between md:px-20 md:border-b-myGreen md:border-b-[3px]">
+                <div className="flex flex-col gap-5">
+                    <div>
+                        <Image src={'/footer_logo.png'} alt="footer logo" width={309} height={56} className="md:w-[630px] md:h-[94px] "/>
+                    </div>
+                    <div className="flex gap-5">
+                        <div className="w-[33.5px] md:h-[61px] md:w-[61px] h-[33.5px] rounded-lg bg-green-100 flex items-center justify-center">
+                            <a href="www.discord.com"><FaDiscord className="md:w-[31px] md:h-[24px] "/></a>
+                        </div>
+                        <div className="w-[33.5px] md:h-[61px] md:w-[61px] h-[33.5px] rounded-lg bg-green-100 flex items-center justify-center">
+                            <a href="www.twitter.com"><FaTwitter className="md:w-[31px] md:h-[24px] "/></a>
+                        </div>
+                        <div className="w-[33.5px] md:h-[61px] md:w-[61px] h-[33.5px] rounded-lg bg-green-100 flex items-center justify-center">
+                            <a href="www.youtube.com"><FaYoutube className="md:w-[31px] md:h-[24px] "/></a>
+                        </div>
+                    </div>
                 </div>
-                <div className="w-[33.5px] h-[33.5px] rounded-lg bg-green-100 flex items-center justify-center">
-                    <a href="www.twitter.com"><FaTwitter /></a>
-                </div>
-                <div className="w-[33.5px] h-[33.5px] rounded-lg bg-green-100 flex items-center justify-center">
-                    <a href="www.youtube.com"><FaYoutube /></a>
-                </div>
-            </div>
-            <div className="pt-4 font-rubik flex gap-4 justify-center border-b-myGreen border-b-[2px] pb-10">
-                <div className="flex flex-col gap-3">
-                    <h1 className="pb-3 font-[600] text-[13.75px] leading-[15.81px] ">Quick Links</h1>
-                    <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] ">Home</a>
-                    <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] ">Earn</a>
-                    <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] ">Rewards</a>
-                    <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] ">Learderships</a>
-                    <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] ">Withdrawals</a>
-                </div>
-                <div className="flex flex-col gap-3">
-                    <h1 className="pb-3 font-[600] text-[13.75px] leading-[15.81px] ">Support</h1>
-                    <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] ">Discord Server</a>
-                    <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] ">Eamil Us</a>
-                </div>
-                <div className="flex flex-col gap-3">
-                    <h1 className="pb-3 font-[600] text-[13.75px] leading-[15.81px] ">Legal</h1>
-                    <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] ">Privacy Policy</a>
-                    <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] ">Terms of Service</a>
-                    <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] ">Cookie Policy</a> 
+                <div className="pt-4 font-rubik flex gap-4 justify-between pb-10 md:w-[681px] md:border-b-0 border-b-myGreen border-b-[2px]">
+                    <div className="flex flex-col gap-3 md:items-end ">
+                        <h1 className="pb-3 font-[600] text-[13.75px] leading-[15.81px] md:text-[25px] md:leading-[28.75px] ">Quick Links</h1>
+                        <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] md:text-[22px] md:leading-[25.3px] ">Home</a>
+                        <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] md:text-[22px] md:leading-[25.3px] ">Earn</a>
+                        <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] md:text-[22px] md:leading-[25.3px] ">Rewards</a>
+                        <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] md:text-[22px] md:leading-[25.3px] ">Learderships</a>
+                        <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] md:text-[22px] md:leading-[25.3px] ">Withdrawals</a>
+                    </div>
+                    <div className="flex flex-col gap-3 md:items-end ">
+                        <h1 className="pb-3 font-[600] text-[13.75px] leading-[15.81px] md:text-[25px] md:leading-[28.75px] ">Support</h1>
+                        <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] md:text-[22px] md:leading-[25.3px] ">Discord Server</a>
+                        <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] md:text-[22px] md:leading-[25.3px] ">Eamil Us</a>
+                    </div>
+                    <div className="flex flex-col gap-3 md:items-end ">
+                        <h1 className="pb-3 font-[600] text-[13.75px] leading-[15.81px] md:text-[25px] md:leading-[28.75px] ">Legal</h1>
+                        <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] md:text-[22px] md:leading-[25.3px] ">Privacy Policy</a>
+                        <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] md:text-[22px] md:leading-[25.3px] ">Terms of Service</a>
+                        <a href="#" className="opacity-75 text-[12.1px] leading-[13.92px] font-[500] md:text-[22px] md:leading-[25.3px] ">Cookie Policy</a> 
+                    </div>
                 </div>
             </div>
 
-            <div className="font-rubik">
-                <p className="font-bold text-[13.75px] pb-2 text-[#161c18]">© 2024 Bloxreward™. All Rights Reserved.</p>
-                <p className="opacity-75 text-[12.1px] font-[400] ">We’re not affiliated in any way with the Roblox Corporation or any of its trademarks.</p>
+            <div className="font-rubik md:flex md:justify-between md:items-center md:pb-10">
+                <p className="font-bold text-[13.75px] pb-2 text-[#161c18] md:text-[25px] md:leading-[28.75px] ">© 2024 Bloxreward™. All Rights Reserved.</p>
+                <p className="opacity-75 text-[12.1px] font-[400] md:text-[22px] md:leading-[25.3px]">We’re not affiliated in any way with the Roblox Corporation or any of its trademarks.</p>
             </div>
         </div>
     )
