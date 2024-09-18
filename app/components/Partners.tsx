156f92a6a84f00263f80dfc60331a82077fd719a
Raghu Anand <raghuaanand@gmail.com>
2024-09-18 23:02:40 +0530

    partners comp done

--- app/components/Partners.tsx
+++ app/components/Partners.tsx
@@ -18,27 +18,32 @@ export default function Partners() {
                 <ul className="grid grid-cols-2 md:grid-cols-4 md:place-content-center place-items-center md:gap-x-5 md:gap-y-5 gap-x-3 gap-y-3 md:mt-10">
                     {partners.map((partner, index) => {
                         const isActive = activeIndex === index;
-                        return (
-                            <li 
-                                key={index} 
-                            
-                                className={`rounded-lg border flex flex-col justify-center items-center h-fit md:h-[300px] w-[148px] md:w-[311px] ${isActive ? 'border-myGreen border-[3px]' : 'bg-white'}`} 
 
+                        // Conditionally apply grid column based on index
+                        let gridColumnClass = '';
+                        if (index === 4) {
+                            gridColumnClass = 'md:col-start-2'; // Start below the 2nd column
+                        } else if (index === 5) {
+                            gridColumnClass = 'md:col-start-3'; // Start below the 3rd column
+                        }
+
+                        return (
+                            <li
+                                key={index}
+                                className={`rounded-lg border flex flex-col justify-center items-center h-fit md:h-[300px] w-[148px] md:w-[311px] ${isActive ? 'border-myGreen border-[3px]' : 'bg-white'} ${gridColumnClass}`}
                                 onClick={() => handleCardClick(index)}
                             >
-
                                 <div className="border-b-myGreen border-b-[1px] w-full flex items-center justify-center">
                                     <Image src={partner.logo} alt="company logo" width={148} height={95.7} layout="responsive" className="w-full" />
                                 </div>
-                                <div className={` flex flex-col items-start px-3 py-2 justify-center font-rubik font-[500] w-[148px] md:w-[311px] md:h-[126px] md:py-4 md:px-4 h-[78.74px] gap-1 ${isActive ? 'bg-myGreen text-white' : 'bg-white text-[#161c18]'} `}>
-                                    <p className="text-[11.54px] leading-[15.69px] md:text-[20px] md:leading-[28.53px] md:font-semibold ">{partner.name} </p>
+                                <div className={`flex flex-col items-start px-3 py-2 justify-center font-rubik font-[500] w-[148px] md:w-[311px] md:h-[126px] md:py-4 md:px-4 h-[78.74px] gap-1 ${isActive ? 'bg-myGreen text-white' : 'bg-white text-[#161c18]'}`}>
+                                    <p className="text-[11.54px] leading-[15.69px] md:text-[20px] md:leading-[28.53px] md:font-semibold">{partner.name}</p>
                                     <p className="opacity-75 leading-[10.38px] text-[9.44px] md:text-[17.16px] md:leading-[18.88px]">{partner.description}</p>
                                 </div>
                             </li>
                         );
                     })}
                 </ul>
-
 
             </div>
         </div>
