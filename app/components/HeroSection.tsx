f1c004a363c7aaa9b14ede6ee1cf2424e0275a97
Raghu Anand <raghuaanand@gmail.com>
2024-09-19 09:11:55 +0530

    still trying

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,31 +1,27 @@
-import Image from "next/image";
-export default function HeroSection() {
+import LgHero from "./lg_hero_animated";
+import SmHeroAnimated from "./sm_hero_animated";
+
+export default function HeroSection(){
     return (
-        <div className="relative pt-[8rem]  md:w-screen h-[54rem] flex flex-col md:flex-row  md:p-0 md:flex md:items-center md:h-[1006px] overflow-hidden">
-            <div className="bg-[url('/bg_grid.svg')] absolute w-full h-full opacity-20 bg-green-100 z-0">
+        <div className="relative h-[54.3125rem] overflow-hidden  pt-[8rem] px-[2rem] md:w-screen md:pt-[15rem] border-b-myGreen border-b-[3px]">
+            <div className="z-20 flex md:w-[50%] flex-col gap-5 md:pl-[4rem]">
 
-            </div>
-            <div className="md:pl-40 h-[35%] md:w-[50%] px-[1.5rem] z-20">
-                <h1 className="font-bold text-[44.55px] md:text-[81px] md:w-[912px] font-rubik leading-[44.55px] md:leading-[81px] md:font-extrabold">
-                    GET <span className="text-myGreen pr-10 md:pr-0 underline decoration-[2px]">FREE ROBUX </span> ANYTIME ANYWHERE!
+                <h1 className="font-extrabold text-[2.784375rem] leading-[2.794375rem] md:text-[5rem] md:leading-[5rem]">
+                    GET <span className="text-myGreen underline decoration-[3px]"> FREE ROBUX </span> ANYTIME ANYWHERE!
                 </h1>
-                <p className="font-rubik font-normal text-[13.2px] md:text-[24px] md:leading-[32.64px] leading-[17.95px] py-3 pr-3 text-[#161c18] opacity-75 md:w-[912px]">Earn Robux effortlessly by completing simple tasks and surveys. No more begging parents for Robux—start earning today!</p>
 
-                <button className=" bg-myGreen h-[33px] md:h-[56px] w-[10rem] md:w-[292px] font-[900] text-center flex items-center justify-center rounded-lg py-[9px] px-[15px] text-[13px] md:text-[22px] md:font-extrabold text-white font-rubik shadow-[0px_4px_0px_0px_rgba(33,149,66,1),0px_2px_1px_0px_rgba(255,255,255,0.24)_inset,0px_-2px_1px_0px_rgba(0,0,0,0.24)_inset] transition-colors duration-300 hover:bg-green-400 " >
+                <p className="font-semibold text-[13.2px] leading-[17.95px] md:text-[24px] md:leading-[32.64px]  opacity-75">
+                    Earn Robux effortlessly by completing simple tasks and surveys. No more begging parents for Robux—start earning today!
+                </p>
+
+                <button className="bg-myGreen h-[2rem] w-fit px-[1rem] rounded-lg font-extrabold shadow-[0px_4px_0px_0px_rgba(33,149,66,1),0px_2px_1px_0px_rgba(255,255,255,0.24)_inset,0px_-2px_1px_0px_rgba(0,0,0,0.24)_inset] text-white transition-colors duration-300 hover:bg-green-400      md:h-[3.5rem] md:text-[22px] ">
                     GET ME STARTED!
                 </button>
             </div>
-            <div className="relative text-red-900   w-[100%] md:w-[50%] md:h-[1034px] ">
-                
+            <div className="">
+                <LgHero />
+                <SmHeroAnimated />
             </div>
-
-            <div className="absolute -bottom-[5rem] w-screen h-[8rem] bg-bgColor border-t-myGreen border-t-[3px] z-30">
-
-            </div>
-
-            {/* <div className="md:h-[13500px] bg-center md:w-[13500px] w-[5000px] h-[5000px] top-[96%] -right-[625%] rounded-full border-myGreen md:border-[5px] border-[3px] absolute md:top-[85%] md:-right-[300%] bg-bgColor z-30">
-
-            </div> */}
         </div>
     )
 }
--- app/components/sm_hero_animated.tsx
+++ app/components/sm_hero_animated.tsx
@@ -1,6 +1,35 @@
+import Image from "next/image";
+
 export default function SmHeroAnimated(){
     return(
-        <div className="md:hidden">
+        <div className="md:hidden relative flex ">
+            <div className="w-screen absolute -left-[11rem] z-30">
+                <Image 
+                    src={'/hero_character.svg'} 
+                    alt="hero character" 
+                    width={544} 
+                    height={543}
+
+                    className="min-w-[35rem] z-30"
+                />
+            </div>
+            <Image 
+                src={'/hero_v3.svg'} 
+                alt="hero vector" 
+                height={621} 
+                width={601}
+
+                className="z-0 min-w-[25rem] absolute -left-[1rem] top-[3rem] rotate-[60deg] opacity-55"
+            />
+
+            <Image 
+                src={'/hero_v2.svg'} 
+                alt="hero vector" 
+                height={65} 
+                width={62}
+
+                className="z-20  absolute right-[0.25rem] top-[20rem] rotate-[22deg]"
+            />
 
         </div>
     )
